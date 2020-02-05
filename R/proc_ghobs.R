7aaacf84ba1fd725eb2a86b8619d6bc02eb5e33b
vanichols <vnichols@iastate.edu>
2020-02-05 12:19:27 -0600

    fix pfifun_xxx, more intuitive

--- R/proc_ghobs.R
+++ R/proc_ghobs.R
@@ -1,13 +1,14 @@
 
-#' pfifun_sumwdsp
+#' pfifun_sum_weed
 #'
 #' @param rawdata Defaults to pfi_ghobsraw
 #'
 #' @return A tibble with total seeds by weed and experimental unit (eu)
 #' @export
-pfifun_sumwdsp <- function(rawdata){
+pfifun_sum_weed <- function(rawdata){
 
-  assertthat::assert_that("SOLPT" %in% colnames(rawdata),
+  assertthat::assert_that(is.data.frame(rawdata), msg = "You must feed me a data.frame")
+  assertthat::assert_that("AMATU" %in% colnames(rawdata),
                           msg = "You can only feed me the pfi_ghobsraw data.")
 
   #--sum by ind species and eu
@@ -36,21 +37,39 @@ pfifun_sumwdsp <- function(rawdata){
 }
 
 
-#' pfifun_sumeu
+#' pfifun_sum_eu
 #'
-#' @param sumdata Output from pfi_sumwdsp function
+#' @param rawdata Output from pfi_sumwdsp function
 #'
 #' @return A tibble with total seeds per eu
 #' @export
-pfifun_sumeu <- function(sumdata) {
+pfifun_sum_eu <- function(rawdata) {
 
-  assertthat::assert_that(is_tibble(sumdata), msg = "You must feed me a tibble")
-  assertthat::assert_that("weed" %in% colnames(sumdata),
-                          msg = "Did you run pfifun_sumsdsp?\nOnly the output from that function will work here.")
+  assertthat::assert_that(is.data.frame(rawdata), msg = "You must feed me a data.frame")
+  assertthat::assert_that("AMATU" %in% colnames(rawdata),
+                          msg = "You can only feed me the pfi_ghobsraw data.")
 
-  #--sum total seeds per eu
+  #--sum by ind species and eu
   datsum <-
-    sumdata %>%
+    rawdata %>%
+    # sum over dates
+    dplyr::mutate_if(is.numeric, tidyr::replace_na, 0) %>%
+    dplyr::group_by(site_name, sys_trt, crop_2019, cc_trt, rep, repID) %>%
+    dplyr::summarise_if(is.numeric, sum) %>%
+    tidyr::pivot_longer(
+      data = .,
+      cols = -(site_name:repID),
+      names_to = "weed",
+      values_to = "seeds") %>%
+    # 2.8575 cm radius cores, 20 cores per 'rep'
+    dplyr::mutate(seeds_m2 = seeds / ( ( (pi * 2.8575^2) * 20 ) / 10000 )) %>%
+    # fix mislabeled weed abbs (they were mislabeled consistently at least)
+    dplyr::mutate(weed = dplyr::recode(weed,
+                                       "SOPT7" = "SOLPT",
+                                       "HPPVU" = "CONCA",
+                                       "PALVA" = "POLAV",
+                                       "EUPMA" = "EPHMA")) %>%
+    dplyr::ungroup() %>%
     dplyr::group_by(site_name, sys_trt, crop_2019, cc_trt, rep, repID) %>%
     dplyr::summarise(totseeds = sum(seeds, na.rm = T),
               totseeds_m2 = sum(seeds_m2, na.rm = T))
