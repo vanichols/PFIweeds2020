e060d261e8688675b77963a2699e513533f55c2c
Gina <vnichols@iastate.edu>
2020-02-20 13:29:53 -0600

    fix duplicate problem, add trayID

--- data-raw/rd_GHobs.R
+++ data-raw/rd_GHobs.R
@@ -1,3 +1,6 @@
+# updated feb 20 2020 (fixing duplicates, 20190503 was in two raw datasheets)
+
+
 # libraries ---------------------------------------------------------------
 
 library(tidyverse)
@@ -5,6 +8,7 @@ library(lubridate)
 library(janitor)
 library(readxl)
 library(fs)
+library(glue)
 
 #--when they updated tidyr, they broke my function (https://rdrr.io/cran/tidyr/man/nest_legacy.html)
 library(tidyr)
@@ -13,6 +17,7 @@ unnest <- unnest_legacy
 
 
 # raw data ----------------------------------------------------------------
+pfi_eus
 
 rawdat <-
   tibble(files = dir_ls("data-raw/raw_GHobs")) %>%
@@ -24,81 +29,88 @@ rawdat <-
 
 
 # wrangle -----------------------------------------------------------------
-
 #--wow I was not consistent :|
 
 
-dat <-
+# 1. no changes, just making characters consistently lowercase and filling in dates-------------------
+
+dat1 <-
   rawdat %>%
   remove_empty("rows") %>%
-  # remove un-needed cols
-  select(-check, -tray, -obs_initials, -electrec_initials) %>%
   # make sure date is consistent
   mutate(obs_date = ymd(obs_date)) %>%
-    mutate_if(is.character, tolower) %>% #--they aren't consistent w/capitalization
-    mutate_if(is.character, str_replace_all, " ", "") %>%  #--sometimes they put spaces
-      # drop down obs_date
-  fill(obs_date) %>%
-  # make things consistent with pfi_eus
+  mutate_if(is.character, tolower) %>% #--they aren't consistent w/capitalization
+  mutate_if(is.character, str_replace_all, " ", "") %>%  #--sometimes they put spaces
+  # drop down obs_date
+  fill(obs_date, obs_initials, electrec_initials)
+
+
+
+# 2. make col names consistent w/pfi_eus  ---------------------------------
+# site_name, field, sys_tr, cc_trt, crop_2019, rep, blockID
+
+dat2 <-
+  dat1 %>%
+  # get site name
+  rename(site_name = location) %>%
   mutate(
-    location = str_to_title(location),
-    location = recode(location,
-                           "Funke" = "Funcke"),
-         crop_2019 = str_sub(crop_trt2019, 1, 1), #--extract crop
-         crop_2019 = recode(crop_2019,
-                            "c" = "corn",
-                            "s" = "soy"),
-         sys_trt = parse_character(str_sub(crop_trt2019, 2)), #--extract system
-         sys_trt = ifelse(is.na(sys_trt), "grain", "silage"),
-         rep = parse_number(cc_trt),
-         cc_trt = tolower(str_sub(cc_trt, 1, -2)),
-         site_name = location,
-         site_name = ifelse( site_name == "Boyd" & crop_2019 == "corn",
-                             "Boyd44", site_name),
-         site_name = ifelse( site_name == "Boyd" & crop_2019 == "soy",
-                             "Boyd42", site_name)) %>%
-    # create repIDs that match the pfi_eus
-    mutate(repID = paste0(str_sub(site_name, 1, 1),
-                          #str_sub(sys_trt, 1, 1),
-                          #str_sub(crop_2019, 1, 1),
-                          "_",
-                          rep)) %>%
-    mutate(coop_name = case_when(
-      grepl("Boyd", site_name) ~ "Kohler",
-      grepl("Funcke", site_name) ~ "Funcke",
-      grepl("Stout", site_name) ~ "Stout")) %>%
-    # remove old rows
-    select(-location, -crop_trt2019) %>%
-    select(obs_date, site_name,
-           coop_name, sys_trt,
-           crop_2019, cc_trt,
-           rep, repID, everything()) %>%
-    arrange(obs_date, site_name, sys_trt, cc_trt, rep)
-
-
-# get into same format as eus ---------------------------------------------
-
-td_ghobs <-
-  dat %>%
-  #--make a site_name and field separately
+    site_name = str_to_title(site_name),
+    site_name = recode(site_name,
+                      "Funke" = "Funcke")) %>%
+  # fix crop_2019
+  mutate(
+    crop_2019 = str_sub(crop_trt2019, 1, 1), #--extract crop
+    crop_2019 = recode(crop_2019,
+                       "c" = "corn",
+                       "s" = "soy")) %>%
+  # get rep from cc_trt
+  mutate(
+    rep = parse_number(cc_trt)) %>%
+  # fix cc_trt
+  mutate(cc_trt = tolower(str_sub(cc_trt, 1, -2))) %>%
+  # get sys_trt
+    mutate(
+      sys_trt = parse_character(str_sub(crop_trt2019, 2)), #--extract system
+      sys_trt = ifelse(is.na(sys_trt), "grain", "silage")) %>%
+  select(-crop_trt2019) %>%
+  mutate(coop_name = case_when(
+    grepl("Boyd", site_name) ~ "Kohler",
+    grepl("Funcke", site_name) ~ "Funcke",
+    grepl("Stout", site_name) ~ "Stout")) %>%
+  # make field
+  # same as site_name except for Boyd, where B44 is 2019 corn field, B42 is 2019 soy field
   mutate(field = site_name,
-         site_name = str_replace_all(site_name, "[:digit:]", ""),
-         fieldtmp = ifelse(grepl("Boyd", field),
-                           paste0("B", parse_number(field)),
-                           str_sub(field, 1, 1))) %>%
-  mutate(blockID = paste0(fieldtmp,
-                        "_",
-                        rep)) %>%
-  select(obs_date, site_name, fieldtmp,
-         sys_trt, cc_trt,
-         crop_2019,
-         rep,
-         blockID,
-         everything(),
-         -repID, -coop_name, -crop_2019, -field) %>%
-  rename(field = fieldtmp)
+         field = ifelse(site_name == "Boyd" & crop_2019 == "corn",
+                      "B44", field),
+         field = ifelse( site_name == "Boyd" & crop_2019 == "soy",
+                    "B42", field)) %>%
+  # blockID is field_rep
+  mutate(blockID = paste0(field, "_", rep))
 
-pfi_ghobsraw <- td_ghobs
+
+# 3. make each tray unique ------------------------------------------------
+
+dat3 <-
+  dat2 %>%
+  mutate(tray = paste0("t", tray),
+         trayID = paste(blockID, cc_trt, tray, sep = "-")) %>%
+  select(-check, -tray) %>%
+  select(obs_date, obs_initials, electrec_initials,
+         coop_name, site_name, field, sys_trt, cc_trt, crop_2019, rep, blockID, trayID,
+         everything())
+
+# 4. remove duplicates ----------------------------------------------------
+
+# check for dupes
+dp <- get_dupes(dat3)
+# that one is a copy-paste error I think. eliminate it
+
+
+dat4 <-
+  dat3 %>% distinct()
+
+
+pfi_ghobsraw <- dat4
 
 usethis::use_data(pfi_ghobsraw, overwrite = TRUE)
 
