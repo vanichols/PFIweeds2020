519709cc71be88d1b16512a1c9f49262c242e1b0
vanichols <vnichols@iastate.edu>
2020-07-14 14:05:53 -0500

    update weeds list to UM and UD from UG and UB

--- data-raw/pfi_weedsplist/pfi_weedsplist.R
+++ data-raw/pfi_weedsplist/pfi_weedsplist.R
@@ -1,6 +1,25 @@
 # updated march 23 2020 (change to jarad-style file structure)
+# july 14 2020 changed UB to UD and UG to UM
 
-pfi_weedsplist <- read_csv("data-raw/pfi_weedsplist/weed-spp-list.csv")
+library(tidyverse)
+
+
+pfi_weedsplist <-
+  read_csv("data-raw/pfi_weedsplist/weed-spp-list.csv") %>%
+  mutate(
+    common_name =
+      recode(
+        common_name,
+        "Unknown grass" = "Unknown monocotyledon",
+        "Unknown broadleaf" = "Unknown dicotyledon"
+      ),
+    code =
+      recode(code,
+             "UG" = "UM",
+             "UB" = "UD")
+  )
+
+
 
 pfi_weedsplist %>% write_csv("data-raw/pfi_weedsplist/pfi_weedsplist.csv")
 usethis::use_data(pfi_weedsplist, overwrite = TRUE)
