6b92ed973ef1872eff9c93e912fa8c4c789457f7
vanichols <vnichols@iastate.edu>
2020-07-15 12:32:26 -0500

    add pfi_sitemgmt

---
+++ data-raw/pfi_sitemgmt/pfi_sitemgmt.R
@@ -0,0 +1,12 @@
+library(usethis)
+library(readxl)
+library(readr)
+library(dplyr)
+
+pfi_sitemgmt <-
+  readxl::read_excel("data-raw/pfi_sitemgmt/PFIweeds_site_summaries-nice.xlsx", skip = 1)
+
+pfi_sitemgmt %>% write_csv("data-raw/pfi_sitemgmt/pfi_sitemgmt.csv")
+usethis::use_data(pfi_sitemgmt, overwrite = TRUE)
+
+
