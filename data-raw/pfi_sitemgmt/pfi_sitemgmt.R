982d2f209f0dd2f805d6020adb17d1f800b79ebb
vanichols <vnichols@iastate.edu>
2020-08-03 11:14:23 -0500

    added rmlist to sitemgmt

--- data-raw/pfi_sitemgmt/pfi_sitemgmt.R
+++ data-raw/pfi_sitemgmt/pfi_sitemgmt.R
@@ -4,11 +4,14 @@ library(readr)
 library(dplyr)
 library(stringr)
 
+rm(list = ls())
+
 pfi_sitemgmt <-
   readxl::read_excel("data-raw/pfi_sitemgmt/PFIweeds_site_summaries-nice.xlsx", skip = 1) %>%
   mutate_all(funs(stringr::str_replace(., "corn", "maize")))
 
-pfi_sitemgmt %>% write_csv("data-raw/pfi_sitemgmt/pfi_sitemgmt.csv")
+pfi_sitemgmt %>%
+  write_csv("data-raw/pfi_sitemgmt/pfi_sitemgmt.csv")
 usethis::use_data(pfi_sitemgmt, overwrite = TRUE)
 
 
